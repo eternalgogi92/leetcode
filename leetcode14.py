24fac53bc0c8b10addce1db8bbd6cb8519dea860
Timmy Qiao <58721314+eternalgogi92@users.noreply.github.com>
2019-12-13 00:22:22 +0800

    Update leetcode14.py

--- leetcode14.py
+++ leetcode14.py
@@ -1,7 +1,7 @@
 # -*- encoding = utf-8 -*-
 # Leetcode-14
 # 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
-# 首先先排序，时间复杂度为NLogN,之后再用双指针的方法，慢慢逼近target
+# 首先先排序，时间复杂度为NLogN,之后再用双指针的方法，慢慢逼近target,时间复杂度为N^2,总的时间复杂度为N^2
 # 来源：力扣（LeetCode）
 # 链接：https://leetcode-cn.com/problems/3sum-closest
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
